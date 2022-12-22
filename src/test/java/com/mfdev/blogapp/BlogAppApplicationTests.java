2510187a957fcbda9da3a02998511d15ed2829e7
mfdev17 <maximsushkin02@gmail.com>
2022-12-22 05:29:34 +0400

    init

---
+++ src/test/java/com/mfdev/blogapp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfdev.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
