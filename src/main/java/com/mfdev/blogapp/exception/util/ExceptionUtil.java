57531e6feeef960ed735bd04e40fd72bf528ddb8
mfdev17 <maximsushkin02@gmail.com>
2022-12-24 22:50:18 +0400

    Added exception handler for unique values

---
+++ src/main/java/com/mfdev/blogapp/exception/util/ExceptionUtil.java
@@ -0,0 +1,21 @@
+package com.mfdev.blogapp.exception.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+public class ExceptionUtil {
+
+  public static ResponseEntity<String> uniqueExceptionHandler(DataIntegrityViolationException e) {
+    String errMessage = Objects.requireNonNull(e.getRootCause()).getMessage();
+    String key = StringUtils
+            .capitalize(StringUtils.substringBetween(errMessage, "(", ")"));
+
+    String value = StringUtils.substringBetween(errMessage, "=(", ")");
+    String response = String.format("%s '%s' already exists", key, value);
+
+    return ResponseEntity.badRequest().body(response);
+  }
+}
