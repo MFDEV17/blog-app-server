fdb349103d763f77ff75b287d7201dcaf5c7c85b
mfdev17 <maximsushkin02@gmail.com>
2022-12-26 13:11:20 +0400

    Added enum converters

---
+++ src/main/java/com/mfdev/blogapp/service/util/enumconverter/JsonConverter.java
@@ -0,0 +1,43 @@
+package com.mfdev.blogapp.service.util.enumconverter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class JsonConverter implements AttributeConverter<Map<String, Object>, String> {
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @Override
+  public String convertToDatabaseColumn(Map<String, Object> attribute) {
+    String content = null;
+
+    try {
+      content = mapper.writeValueAsString(attribute);
+    } catch (JsonProcessingException e) {
+      log.error(e.getMessage());
+    }
+
+    return content;
+  }
+
+  @Override
+  public Map<String, Object> convertToEntityAttribute(String dbData) {
+    Map<String, Object> contentJson = null;
+
+    try {
+      contentJson = mapper.readValue(dbData, new TypeReference<HashMap<String, Object>>() {
+      });
+    } catch (IOException e) {
+      log.error(e.getMessage());
+    }
+
+    return contentJson;
+  }
+}
--- src/main/java/com/mfdev/blogapp/service/util/enumconverter/RoleConverter.java
+++ src/main/java/com/mfdev/blogapp/service/util/enumconverter/RoleConverter.java
@@ -1,10 +1,11 @@
-package com.mfdev.blogapp.service.util;
+package com.mfdev.blogapp.service.util.enumconverter;
 
 import com.mfdev.blogapp.entity.user.authority.Role;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class RoleConverter implements Converter<String, Role> {
   @Override
   public Role convert(String source) {
