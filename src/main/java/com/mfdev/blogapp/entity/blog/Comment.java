a26aff215d8f04aeeb650e508bf929fab3db4a50
mfdev17 <maximsushkin02@gmail.com>
2022-12-26 23:45:54 +0400

    changed comment property to map

--- src/main/java/com/mfdev/blogapp/entity/blog/Comment.java
+++ src/main/java/com/mfdev/blogapp/entity/blog/Comment.java
@@ -1,7 +1,7 @@
 package com.mfdev.blogapp.entity.blog;
 
-import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.mfdev.blogapp.entity.user.User;
+import com.mfdev.blogapp.service.util.enumconverter.JsonConverter;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +10,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
+import java.util.Map;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -22,9 +23,9 @@ public class Comment {
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
 
-  @Column(columnDefinition = "json")
-  @JsonRawValue
-  private String comment;
+  @Convert(converter = JsonConverter.class)
+  @Column(columnDefinition = "text")
+  Map<String, Object> comment;
 
   @ManyToOne
   private User user;
