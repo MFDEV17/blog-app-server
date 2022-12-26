d3f4a860d5a0c5bbeb6ba2837178637538e00420
mfdev17 <maximsushkin02@gmail.com>
2022-12-26 23:45:09 +0400

    added controllers for likes entities

---
+++ src/main/java/com/mfdev/blogapp/controller/LikeController.java
@@ -0,0 +1,28 @@
+package com.mfdev.blogapp.controller;
+
+import com.mfdev.blogapp.dto.CommentLikeDTO;
+import com.mfdev.blogapp.dto.bloglike.BlogLikeDTO;
+import com.mfdev.blogapp.service.BlogLikeService;
+import com.mfdev.blogapp.service.CommentLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LikeController {
+  private final BlogLikeService blogLikeService;
+  private final CommentLikeService commentLikeService;
+
+  @PostMapping("/like-post")
+  public ResponseEntity<?> likePost(@RequestBody BlogLikeDTO dto) {
+    return blogLikeService.setLike(dto);
+  }
+
+  @PostMapping("/like-comment")
+  public ResponseEntity<?> likeComment(@RequestBody CommentLikeDTO dto) {
+    return commentLikeService.likeComment(dto);
+  }
+}
---
+++ src/main/java/com/mfdev/blogapp/service/CommentLikeService.java
@@ -0,0 +1,35 @@
+package com.mfdev.blogapp.service;
+
+import com.mfdev.blogapp.dto.CommentLikeDTO;
+import com.mfdev.blogapp.repository.UserRepository;
+import com.mfdev.blogapp.repository.like.CommentLikeRepository;
+import com.mfdev.blogapp.service.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentLikeService {
+  private final UserRepository userRepository;
+  private final SecurityUtil securityUtil;
+  private final CommentLikeRepository commentLikeRepository;
+
+  @PreAuthorize("isFullyAuthenticated()")
+  public ResponseEntity<?> likeComment(CommentLikeDTO dto) {
+    Long sessionUserId = userRepository.getUserId(securityUtil.getSessionUsername());
+
+    if (!isLikeExists(dto)) {
+      commentLikeRepository.likeComment(dto.getCommentId(), sessionUserId);
+    } else {
+      commentLikeRepository.deleteLike(dto.getCommentId(), sessionUserId);
+    }
+
+    return ResponseEntity.ok().build();
+  }
+
+  private boolean isLikeExists(CommentLikeDTO dto) {
+    return commentLikeRepository.isLikeExists(dto.getCommentId(), securityUtil.getSessionUsername());
+  }
+}
