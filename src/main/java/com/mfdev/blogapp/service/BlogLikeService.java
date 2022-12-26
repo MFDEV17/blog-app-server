73381dcd10baf135f66cadad6514cf2685858554
mfdev17 <maximsushkin02@gmail.com>
2022-12-26 23:46:30 +0400

    added like service

---
+++ src/main/java/com/mfdev/blogapp/service/BlogLikeService.java
@@ -0,0 +1,37 @@
+package com.mfdev.blogapp.service;
+
+import com.mfdev.blogapp.dto.bloglike.BlogLikeDTO;
+import com.mfdev.blogapp.repository.like.BlogLikeRepository;
+import com.mfdev.blogapp.repository.UserRepository;
+import com.mfdev.blogapp.service.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BlogLikeService {
+  private final BlogLikeRepository blogLikeRepository;
+  private final UserRepository userRepository;
+  private final SecurityUtil securityUtil;
+
+  @PreAuthorize("isFullyAuthenticated()")
+  public ResponseEntity<?> setLike(BlogLikeDTO dto) {
+    Long userId = userRepository
+            .getUserId(securityUtil.getSessionUsername());
+
+    if (isLikeExists(dto)) {
+      blogLikeRepository.deleteLike(dto.getBlogId(), userId);
+    } else {
+      blogLikeRepository.addLike(dto.getBlogId(), userId);
+    }
+    return ResponseEntity.ok().build();
+  }
+
+  private boolean isLikeExists(BlogLikeDTO dto) {
+    return blogLikeRepository.isLikeExists(
+            dto.getBlogId(),
+            securityUtil.getSessionUsername());
+  }
+}
