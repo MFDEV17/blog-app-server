daf16eb2d3453a89ef0577e6a44e59c2154d8a7f
mfdev17 <maximsushkin02@gmail.com>
2022-12-29 00:11:27 +0400

    added comment service

--- src/main/java/com/mfdev/blogapp/repository/CommentRepository.java
+++ src/main/java/com/mfdev/blogapp/repository/CommentRepository.java
@@ -1,10 +1,33 @@
 package com.mfdev.blogapp.repository;
 
 import com.mfdev.blogapp.entity.blog.Comment;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-  Long countAllByBlogId(Long id);
+
+  @Modifying
+  @Transactional
+  @Query(value = "insert into comment(comment, user_id, blog_id) values (?1, ?2, ?3)", nativeQuery = true)
+  void saveComment(Map<String, Object> comment, Long userId, Long blogId);
+
+
+  @Modifying
+  @Transactional
+  @Query("update Comment c set c.comment = ?1 where c.id = ?2")
+  void updateComment(Map<String, Object> comment, Long commentId);
+
+  @Query("select u.username from users u left join Comment c on u.id = c.user.id where c.id = ?1")
+  Long getCommentCreatorUsername(Long commentId);
+
+  @Modifying
+  @Transactional
+  @Query("delete from Comment c where c.id = ?1")
+  void deleteCommentById(Long id);
 }
---
+++ src/main/java/com/mfdev/blogapp/service/CommentService.java
@@ -0,0 +1,53 @@
+package com.mfdev.blogapp.service;
+
+import com.mfdev.blogapp.dto.comment.CreateCommentDTO.CreateCommentDTO;
+import com.mfdev.blogapp.dto.comment.UpdateCommentDTO;
+import com.mfdev.blogapp.repository.CommentRepository;
+import com.mfdev.blogapp.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+  private final CommentRepository commentRepository;
+  private final SecurityUtil securityUtil;
+
+  @PreAuthorize("isFullyAuthenticated()")
+  public ResponseEntity<?> createComment(CreateCommentDTO dto) {
+    dto.setUserId(securityUtil.getSessionUserId());
+    commentRepository.saveComment(
+            dto.getComment(),
+            dto.getUserId(),
+            dto.getBlogId()
+    );
+
+    return ResponseEntity.ok("Comment has been created");
+  }
+
+  @PreAuthorize(
+          "isFullyAuthenticated() and " +
+                  "authentication.name" +
+                  ".equals(@commentRepository.getCommentCreatorUsername(#dto.commentId)) " +
+                  "or hasAuthority('SCOPE_'" +
+                  ".concat(T(com.mfdev.blogapp.entity.user.authority.Authority)" +
+                  ".EDIT_OTHER_COMMENT))")
+  public ResponseEntity<?> updateComment(UpdateCommentDTO dto) {
+    commentRepository.updateComment(dto.getComment(), dto.getCommentId());
+    return ResponseEntity.ok("Comment has been edited");
+  }
+
+  @PreAuthorize(
+          "isFullyAuthenticated() and " +
+                  "authentication.name" +
+                  ".equals(@commentRepository.getCommentCreatorUsername(#commentId)) " +
+                  "or hasAuthority('SCOPE_'" +
+                  ".concat(T(com.mfdev.blogapp.entity.user.authority.Authority)" +
+                  ".DELETE_OTHER_COMMENT))")
+  public ResponseEntity<?> deleteComment(Long commentId) {
+    commentRepository.deleteCommentById(commentId);
+    return ResponseEntity.ok("Comment has been deleted");
+  }
+}
