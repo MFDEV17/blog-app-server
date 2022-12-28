a433687a17ae984f924b2ba77dff87eeec4e9916
mfdev17 <maximsushkin02@gmail.com>
2022-12-29 00:14:48 +0400

    Added comment controller

---
+++ src/main/java/com/mfdev/blogapp/controller/CommentController.java
@@ -0,0 +1,30 @@
+package com.mfdev.blogapp.controller;
+
+import com.mfdev.blogapp.dto.comment.CreateCommentDTO.CreateCommentDTO;
+import com.mfdev.blogapp.dto.comment.UpdateCommentDTO;
+import com.mfdev.blogapp.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+public class CommentController {
+  private final CommentService commentService;
+
+  @PostMapping("/create")
+  public ResponseEntity<?> createComment(@RequestBody CreateCommentDTO dto) {
+    return commentService.createComment(dto);
+  }
+
+  @PutMapping("/update")
+  public ResponseEntity<?> updateComment(@RequestBody UpdateCommentDTO dto) {
+    return commentService.updateComment(dto);
+  }
+
+  @DeleteMapping("/delete/{commentId}")
+  public ResponseEntity<?> deleteComment(@PathVariable Long commentId) {
+    return commentService.deleteComment(commentId);
+  }
+}
