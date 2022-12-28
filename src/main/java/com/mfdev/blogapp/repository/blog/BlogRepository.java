04ba53ad9e3318d4a28d2eb51846bf047f296158
mfdev17 <maximsushkin02@gmail.com>
2022-12-28 22:33:27 +0400

    added some new methods for blog service in repository

--- src/main/java/com/mfdev/blogapp/repository/blog/BlogRepository.java
+++ src/main/java/com/mfdev/blogapp/repository/blog/BlogRepository.java
@@ -3,14 +3,15 @@ package com.mfdev.blogapp.repository.blog;
 import com.mfdev.blogapp.dto.blog.ShortBlogDTO;
 import com.mfdev.blogapp.entity.blog.Blog;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
@@ -25,9 +26,19 @@ public interface BlogRepository extends JpaRepository<Blog, Long> {
 
   @Modifying
   @Transactional
+  @Query(nativeQuery = true, value = "insert into blog(content, user_id) values (?1, ?2)")
+  void saveBlogByUserId(Long id, Map<String, Object> content);
+
+  @Modifying
+  @Transactional
   @Query("delete from Blog where id = ?1")
   void deletePost(Long postId);
 
+  boolean existsById(Long id);
+
   @EntityGraph(attributePaths = {"likes", "comments", "user"})
-  Set<ShortBlogDTO> findAllByUserUsername(String username);
+  List<ShortBlogDTO> findAllByUserUsername(String username, Pageable pageable);
+
+  @EntityGraph(attributePaths = {"likes", "comments", "user"})
+  List<ShortBlogDTO> findBy(Pageable pageable);
 }
